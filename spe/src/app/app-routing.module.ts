40615a557e8016b51bf9741d9db01f00aaabb6ad
Mahi1209 <maheedhar.1208@gmail.com>
2023-04-06 14:38:09 +0530

    commit_1

--- spe/src/app/app-routing.module.ts
+++ spe/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'', component:HomePageComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
